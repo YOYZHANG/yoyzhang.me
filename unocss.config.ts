d649f69fe413fb02377fd7a6e04385df22594aa4
zhangxiaoqian <alexuzhang@gmail.com>
2023-12-13 15:06:24 +0800

    chore: init

---
+++ unocss.config.ts
@@ -0,0 +1,21 @@
+import {
+  defineConfig,
+  presetAttributify,
+  presetIcons,
+  presetUno,
+  transformerDirectives,
+} from 'unocss'
+
+export default defineConfig({
+  shortcuts: [
+    // ...
+  ],
+  presets: [
+    presetUno(),
+    presetAttributify(),
+    presetIcons(),
+  ],
+  transformers: [
+    transformerDirectives(),
+  ],
+})
